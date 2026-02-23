261e1e929e3dca228cff700fb38260d4a13d2c17
nicky4gun <nicklas.stagesorensen@outlook.dk>
2026-02-23 13:53:45 +0100

    sql done for now

--- src/main/resources/dk/jannikognicklas/monikasfrisorsalon/sql/schema.sql
+++ src/main/resources/dk/jannikognicklas/monikasfrisorsalon/sql/schema.sql
@@ -1,25 +1,25 @@
 create table employee
 (
     id       int primary key auto_increment,
-    name     varchar(20),
-    username varchar(20),
-    password varchar(10)
+    name     varchar(20) not null,
+    username varchar(20) not null unique,
+    password varchar(10) not null
 
 );
 create table hair_treatment
 (
     id         int primary key auto_increment,
-    name       varchar(40),
-    timeLength int,
-    price double
+    name       varchar(40) not null,
+    timeLength int not null,
+    price double not null
 );
 create table booking
 (
     id                int primary key auto_increment,
-    startdato         datetime,
-    enddato           datetime,
-    employee_id       int,
-    hair_treatment_id int,
+    startdato         datetime not null,
+    enddato           datetime not null,
+    employee_id       int not null,
+    hair_treatment_id int not null,
     foreign key (employee_id) references employee (id),
     foreign key (hair_treatment_id) references hair_treatment (id)
 )
